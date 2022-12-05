b6e3fdb69ac228a7c970603cd3046d6b691c13ba
raymondhle <homan.leung@gammonconstruction.com>
2022-12-05 11:27:48 +0800

    modify dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -12,23 +12,19 @@ import cv2
 
 class MyImageFolder(Dataset):
     def __init__(self, root_dir):
-        super(MyImageFolder, self).__init__()
-        self.data = []
+        super().__init__()
         self.root_dir = root_dir
-        self.class_names = os.listdir(root_dir)
 
-        for index, name in enumerate(self.class_names):
-            files = os.listdir(os.path.join(root_dir, name))
-            self.data += list(zip(files, [index] * len(files)))
+        self.data=sorted(os.listdir(self.root_dir))
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
-        img_file, label = self.data[index]
-        root_and_dir = os.path.join(self.root_dir, self.class_names[label])
+        img_file = self.data[index]
+        root_and_dir = os.path.join(self.root_dir,img_file)
 
-        image = cv2.imread(os.path.join(root_and_dir, img_file))
+        image = cv2.imread(root_and_dir)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         both_transform = config.both_transforms(image=image)["image"]
         low_res = config.lowres_transform(image=both_transform)["image"]
